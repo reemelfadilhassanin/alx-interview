91abd20845f03ad32c14c98fa7059072e4eba5cc
root <root@Reem.localdomain>
2024-10-03 11:12:36 +0300

    task 1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-"""
-This module defines the function to generate Pascal's Triangle.
-"""
-
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing
@@ -12,15 +7,20 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-
+    
     tra = [[1]]  # Start with the first row
 
     for i in range(1, n):
         temp = [1]  # Each row starts with a 1
         for j in range(len(tra[i - 1]) - 1):
-            # Sum the two values from the previous row
             temp.append(tra[i - 1][j] + tra[i - 1][j + 1])
         temp.append(1)  # Each row ends with a 1
-        tra.append(temp)  # Append the constructed row to the triangle
+        tra.append(temp)
 
     return tra
+
+# Using help to check the docstring
+help(pascal_triangle)
+
+# Using __doc__ attribute to check the docstring
+print(pascal_triangle.__doc__)
