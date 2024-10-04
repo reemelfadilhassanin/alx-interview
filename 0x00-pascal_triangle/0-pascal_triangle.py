49b1d0cd6dd3856822e3fc614be32d0f6b18956a
root <root@Reem.localdomain>
2024-10-04 05:21:52 +0300

    configration

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,24 +3,26 @@
 This module defines the function to generate Pascal's Triangle.
 """
 
-def pascal_triangle(n):
-    """
-    Returns a list of lists of integers representing
-    the Pascal’s triangle of n.
-    
-    If n <= 0, return an empty list.
-    """
-    if n <= 0:
-        return []
+from typing import List
 
-    tra = [[1]]  # Start with the first row
+def pascal_triangle(n: int) -> List[List[int]]:
+	"""
+	Returns a list of lists of integers representing
+	the Pascal’s triangle of n.
+	
+	If n <= 0, return an empty list.
+	"""
+	if n <= 0:
+		return []
 
-    for i in range(1, n):
-        temp = [1]  # Each row starts with a 1
-        for j in range(len(tra[i - 1]) - 1):
-            # Sum the two values from the previous row
-            temp.append(tra[i - 1][j] + tra[i - 1][j + 1])
-        temp.append(1)  # Each row ends with a 1
-        tra.append(temp)  # Append the constructed row to the triangle
+	triangle = [[1]]  # Start with the first row
 
-    return tra
+	for i in range(1, n):
+		row = [1]  # Each row starts with a 1
+		for j in range(len(triangle[i - 1]) - 1):
+			# Sum the two values from the previous row
+			row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+		row.append(1)  # Each row ends with a 1
+		triangle.append(row)  # Append the constructed row to the triangle
+
+	return triangle
