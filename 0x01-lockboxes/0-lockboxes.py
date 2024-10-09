d00d4a669b7c308babef2248e85e1c48540bd395
root <root@Reem.localdomain>
2024-10-09 16:48:21 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,25 +3,24 @@
 Solution to lockboxes problem
 """
 
+
 def canUnlockAll(boxes):
     """
     Determines whether a series of locked boxes can be opened
     based on keys that can be attained.
+    Solution to the lockboxes problem
     """
-    if not isinstance(boxes, list) or not boxes:
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
         return False
 
-    n = len(boxes)
-    visited = [False] * n  # Track visited boxes
-    visited[0] = True  # The first box is unlocked
-    stack = [0]  # Start from the first box
-
-    while stack:
-        current_box = stack.pop()
-        
-        for key in boxes[current_box]:
-            if key < n and not visited[key]:  # Check if the key is valid and box is not visited
-                visited[key] = True  # Mark the box as visited
-                stack.append(key)  # Add to stack for further exploration
-
-    return all(visited)  # Check if all boxes have been visited
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
