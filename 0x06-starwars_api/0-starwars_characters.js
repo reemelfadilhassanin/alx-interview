4546bba64df26bd5638d94ea537384b10f89cb5f
root <root@Reem.localdomain>
2024-11-14 01:24:31 +0300

    done

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,40 +1,51 @@
 #!/usr/bin/node
 
+// Require the 'request' module for making HTTP requests
 const request = require('request');
 
-// Get the movie ID from the command line arguments
-const movieId = process.argv[2];
-
-// URL of the Star Wars API film endpoint
-const url = `https://swapi.dev/api/films/${movieId}/`;
-
-// Function to fetch the characters for the given movie
-request(url, function (err, res, body) {
-  if (err) {
-    console.log('Error fetching movie data:', err);
-    return;
-  }
-
-  // Parse the response body to get the list of characters
-  const filmData = JSON.parse(body);
-
-  // Check if the film data is valid
-  if (!filmData.characters || filmData.characters.length === 0) {
-    console.log('No characters found for this movie.');
-    return;
-  }
-
-  // Fetch each character by their URL
-  filmData.characters.forEach(characterUrl => {
-    request(characterUrl, function (err, res, body) {
-      if (err) {
-        console.log('Error fetching character data:', err);
-        return;
-      }
-
-      // Parse the character data and print the character's name
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
+// Function to make a GET request and return a Promise
+function makeRequest(url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) reject(error);
+      else resolve(JSON.parse(body));
     });
   });
-});
+}
+
+// Main function to fetch movie and character data
+async function main() {
+  const args = process.argv;  // Get command-line arguments
+
+  // Check if a movie ID was passed
+  if (args.length < 3) {
+    console.log('Usage: ./0-starwars_characters.js <movie_id>');
+    return;
+  }
+
+  // Define the URL for fetching movie details based on movie ID
+  const movieUrl = `https://swapi-api.alx-tools.com/api/films/${args[2]}`;
+
+  try {
+    // Fetch the movie data
+    const movie = await makeRequest(movieUrl);
+
+    // If there are no characters for this movie, exit early
+    if (!movie.characters || movie.characters.length === 0) {
+      console.log('No characters found for this movie.');
+      return;
+    }
+
+    // For each character URL, fetch character data and print their name
+    for (const characterUrl of movie.characters) {
+      const character = await makeRequest(characterUrl);
+      console.log(character.name);
+    }
+  } catch (error) {
+    // If an error occurs, print it
+    console.error('Error:', error.message);
+  }
+}
+
+// Execute the main function
+main();
